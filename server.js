bce1816d6edce780d9f0096e232a209be14ce4da
Glitch (foremost-clerk) <none>
2020-01-13 14:40:25 +0000

    🍦🏌 Checkpoint ./server.js:1738867/2769

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 
-var cors = require('cors');
+var cors = require("cors");
+
+// DNS core module
+var dns = require("dns");
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
@@ -19,19 +23,28 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
+const urlRegex =
+  "/((([A-Za-z]{3,9}:(?://)?)(?:[-;:&=+$,w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,w]+@)[A-Za-z0-9.-]+)((?:/[+~%/.w-_]*)???(?:[-+=&;%@.w_]*)#?(?:[.!/\\w]*))?)/";
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.post("/api/shorturl/new", (req, res) => {
+  if (urlRegex.test(req.body.original_url)) {
+    let splitUrl = req.body.original_url.split('/');
+    let shortenedUrl = splitUrl.slice(0,)
+  }
+  res.json({"error":"invalid URL"});
+});
+
+app.listen(port, function() {
+  console.log("Node.js listening ...");
 });
